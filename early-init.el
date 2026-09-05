0a631022a547d888072181233ddeef67adba0550
coalooball <cyan_cg@outlook.com>
2026-09-05 20:27:21 +0800

    .

--- early-init.el
+++
@@ -1,7 +0,0 @@
-;;; early-init.el --- Early startup settings -*- lexical-binding: t; -*-
-
-;; Prevent Emacs from activating ELPA packages before init.el has a chance to
-;; disable packages that should come from Emacs itself, such as `project'.
-(setq package-enable-at-startup nil)
-
-;;; early-init.el ends here
