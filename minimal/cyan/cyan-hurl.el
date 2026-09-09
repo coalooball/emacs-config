2987c23e258c2a6786c9a49de8194ea722b9ca8d
coalooball <cyan_cg@outlook.com>
2026-09-09 09:52:40 +0800

    change

--- minimal/cyan/cyan-hurl.el
+++ minimal/cyan/cyan-hurl.el
@@ -7,9 +7,24 @@
 ;;; Code:
 
 (require 'use-package)
+
+(defun my/hurl-run-buffer ()
+  "Run the current Hurl file and show its output in a compilation buffer."
+  (interactive)
+  (unless buffer-file-name
+    (user-error "This buffer is not visiting a file"))
+  (when (buffer-modified-p)
+    (save-buffer))
+  (let ((default-directory (file-name-directory buffer-file-name)))
+    (compile (format "hurl %s"
+                    (shell-quote-argument (file-name-nondirectory
+                                           buffer-file-name))))))
+
 (use-package hurl-mode
   :ensure nil
-  :mode "\\.hurl\\'")
+  :mode "\\.hurl\\'"
+  :bind (:map hurl-mode-map
+              ("C-c C-c" . my/hurl-run-buffer)))
 
 (provide 'cyan-hurl)
 ;;; cyan-hurl.el ends here
